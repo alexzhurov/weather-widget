d919614d9e9c38ab359f4d3a8725691ce9a3d493
au.zhurov <alexzhurovbiz@gmail.com>
2025-12-04 16:00:28 +0200

    feat(config): add weather API configuration constants

---
+++ src/config/weather.ts
@@ -0,0 +1,3 @@
+export const API_KEY = "380943926c11c69a3d48abee172805ef";
+export const API_BASE_URL = "https://api.openweathermap.org";
+export const WEATHER_BASE_URL = `${API_BASE_URL}/data/2.5/weather`;
